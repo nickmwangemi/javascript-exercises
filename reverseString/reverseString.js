359ea004ecb37f213177f64992f18b8c6867c2c3
nickmwangemi <nickmwangemi@gmail.com>
2020-10-29 13:18:43 +0300

    Solve exercises

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+	return (string = '')
+		? ''
+		: (splitString = string.split('').reverse().join(''))
 }
+
+console.log(reverseString('nick'))
 
 module.exports = reverseString
